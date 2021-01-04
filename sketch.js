caa9b46173325ce83666211c53af7d497b5aa1ff
siyasneh <snjha1969@gmail.com>
2021-01-04 17:31:44 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 310, 305, 165);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -41,9 +41,7 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+  
     box1.display();
     box2.display();
     ground.display();
